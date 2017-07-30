0e7d426443009f1cf6772861eea457258bf0646c
sumit23 <ysumit99@gmail.com>
2017-07-30 14:38:05 +0530

    july lunchtime find maximum solution added!

---
+++ cpp/find_max_july_ltime_2017.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+
+	cin>>t;
+
+	while(t--)
+	{
+
+		int ele;
+		std::vector<int> v;
+
+		char space;
+		if(t!=0)
+			{
+				do{
+			scanf("%d%c",&ele,&space);
+
+			v.push_back(ele);
+		}while(space != '\n');
+
+	}
+	else
+	{
+		while (std::cin >> ele)
+		{
+		    v.push_back(ele);
+		}
+
+		
+	}
+
+		int count  = v.size();
+		count--;
+
+		sort(v.rbegin(),v.rend());
+
+		if(v[0] != count)
+			cout<<v[0]<<endl;
+		else
+		{
+			cout<<v[1]<<endl;
+		}
+
+		
+
+	}
+	return 0;
+}
