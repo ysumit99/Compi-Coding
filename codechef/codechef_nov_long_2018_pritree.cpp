979c8fdd5aa02e31b81b8fbb9699f05840790775
ysumit99 <ysumit99@gmail.com>
2018-11-10 07:47:25 +0530

    Nov long Challenge problem solution

---
+++ codechef/codechef_nov_long_2018_pritree.cpp
@@ -0,0 +1,27 @@
+//https://www.codechef.com/NOV18B/problems/PRITREE
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,ele;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+    	cin >> ele;
+
+    }
+
+    for (int i = 0; i < n-1; i+=2)
+    {
+    	cout << i+1 << " " << i+2 << "\n" << i+2 << " " << i+3
+    }
+
+	return 0;
+}
