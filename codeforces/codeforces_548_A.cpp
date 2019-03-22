16d70ad03b986ee77ddf3ce0fe6584864f30cd80
ysumit99 <ysumit99@gmail.com>
2019-03-22 10:08:15 +0530

    Codeforces Round 548 solutions

---
+++ codeforces/codeforces_548_A.cpp
@@ -0,0 +1,30 @@
+//http://codeforces.com/contest/1139/problem/A
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n res = 0;
+
+	cin >> n;
+
+	string str;
+
+	cin >> str;
+
+	for (int i = 0; i < n; ++i)
+	{
+		/* code */
+
+		if((str[i] - '0') % 2 == 0)
+		{
+			res += str[i];
+		} 
+	}
+
+	cout << res;
+
+
+	return 0;
+}
---
+++ codeforces/codeforces_548_B.cpp
@@ -0,0 +1,52 @@
+//http://codeforces.com/contest/1139/problem/B
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n;
+
+	cin >> n;
+
+
+	std::vector<int> v(n);
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v[i];
+	}
+
+
+	int res = 0;
+
+	for (int i = n - 1; i >= 0 ; --i)
+	{
+		if(v[i] <= 0)
+			break;
+
+		if(i == n - 1)
+			{
+				res += v[i];
+				continue;
+			}
+			
+		if(v[i] < v[i+1])
+			{
+				res += v[i];
+			}
+		else if(v[i] >= v[i+1])
+		{
+			res += v[i + 1] - 1;
+			v[i] = v[i + 1] - 1;
+		}
+
+		if(v[i] <= 0)
+			break;
+
+	}
+
+	cout << res;
+	return 0;
+}
