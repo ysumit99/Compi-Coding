7a233c68a4b1e1e0f53efec577d8ea5cd00637c4
ysumit99 <ysumit99@gmail.com>
2019-04-01 01:43:44 +0530

    Codeforces Round 549 Problem C Solution

---
+++ codeforces/Codeforces_549_Problem_C_Respect_Tree.cpp
@@ -0,0 +1,14 @@
+//http://codeforces.com/contest/1143/problem/C
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int N=1e5+10;
+int p[N],c[N],s[N],n,can;
+int main(){
+	cin>>n;
+	for (int i=1;i<=n;i++)scanf("%d%d",&p[i],&c[i]),s[i]=c[i];
+	for (int i=1;i<=n;i++)if (p[i]>0)s[p[i]]&=c[i];
+	for (int i=1;i<=n;i++)if (s[i])printf("%d ",i),can=1;
+	if (!can)printf("-1");
+}
