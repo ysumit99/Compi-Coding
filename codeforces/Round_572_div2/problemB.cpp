b9165a184453ccf5fcd2723d6473fc64fbbc241e
ysumit99 <ysumit99@gmail.com>
2019-07-06 11:44:18 +0530

    prepare for virtual contest

---
+++ codeforces/Round_572_div2/problemB.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/contest/1189/problem/B
+
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main() {
+#ifdef DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base :: sync_with_stdio(false);
+	cin.tie (0);
+
+	
+
+	return 0;
+}
