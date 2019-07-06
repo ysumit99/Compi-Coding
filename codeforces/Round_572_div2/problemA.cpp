97de0400d04761f52aad78385e17784944ec66c7
ysumit99 <ysumit99@gmail.com>
2019-07-06 13:48:41 +0530

    codeforces round 572 div 2 submissions

--- codeforces/Round_572_div2/problemA.cpp
+++ codeforces/Round_572_div2/problemA.cpp
@@ -1,5 +1,5 @@
 //https://codeforces.com/contest/1189/problem/A
-
+//Incomplete / Almost done
 #include<bits/stdc++.h>
 #define ll long long int
 
@@ -13,6 +13,82 @@ int main() {
 	ios_base :: sync_with_stdio(false);
 	cin.tie (0);
 
+    int n;
+
+    cin >> n;
+
+
+    string str;
+
+    cin >> str;
+
+    int i;
+    for(i = n; i >= 1; i--)
+    {
+        int count_1 = 0, count_0 = 0, counter = 0, flag = 0;
+        for(int j = 0; j < n; j++)
+        {
+           
+           if(str[j] == '0')
+                count_0++;
+           else
+                count_1++;
+
+            counter++;
+            if(counter % i == 0)
+            {
+                if(count_1 == count_0)
+                {
+                    flag = 1;
+                    break;
+                }
+                count_0 = 0;
+                count_1 = 0;
+            }
+            
+        }
+        if(flag == 0)
+            {
+                cout << "count_ 0 " << count_0 << " count_1 = " << count_1 << " i = " << i <<  endl; 
+                break;
+            }
+
+    }
+       int step = 0;
+
+       if(n % i == 0)
+       {
+           step =  n/i;
+       }
+       else
+        {
+            step = (n/i) + 1;
+        } 
+        
+        cout << step << "\n";
+
+        int counter = 0;
+
+        for(int j = 0; j < n; j++)
+        {
+            // if(j != 0 && (j+1) % i == 0)
+            // {
+            //     cout << " ";
+            // }
+            
+           
+            counter++;
+             if(j != 0 && counter == i)
+            {
+                
+                    cout << " ";
+                    counter = 0;
+            }
+            cout << str[j];
+        }
+
+
+   
 	
 
 	return 0;
--- codeforces/Round_572_div2/problemB.cpp
+++ codeforces/Round_572_div2/problemB.cpp
@@ -1,4 +1,5 @@
 //https://codeforces.com/contest/1189/problem/B
+//Accepted
 
 #include<bits/stdc++.h>
 #define ll long long int
@@ -13,7 +14,71 @@ int main() {
 	ios_base :: sync_with_stdio(false);
 	cin.tie (0);
 
-	
+    
+	int n;
+
+    cin >> n;
+
+    vector<ll> v(n);
+    vector<ll> v2(n);
+
+    for(int i = 0; i < n ; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.rbegin(),v.rend());
+    int mid = n/2;
+    v2[mid] = v[0];
+
+    int offset = 1;
+    
+    for(int i = 1; i < n; i++)
+    {
+        //if odd place in left side
+        if(i % 2 == 1)
+        {
+            v2[mid - offset] = v[i];
+        }
+        else
+        {
+            if(n % 2 == 0 && i == n-1)
+                continue;
+            v2[mid + offset] = v[i];
+            offset++;
+            
+        }
+    }
+
+    // for(int i = 0; i < n; i++)
+    //     cout << v[i] << " ";
+    // cout << endl;
+
+    //  for(int i = 0; i < n; i++)
+    //     cout << v2[i] << " ";
+    // cout << endl;
+
+    if(v2[0] >= v2[1] + v2[n-1])
+       {
+           cout << "NO";
+           return 0;
+       } 
+
+    for(int i = 1; i < n - 1; i++)
+    {
+        if(v2[i] >= v2[i-1] + v2[i + 1])
+            {
+                cout << "NO";
+                return 0;
+            }
+    }
+
+    cout << "YES\n";
+    for(int i = 0; i < n ; i++)
+    {
+        cout << v2[i] << " ";
+    }
+
 
 	return 0;
 }
