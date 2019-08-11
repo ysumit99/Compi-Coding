a653b72f960443068a8fa0c22f5df0344292f4dd
ysumit99 <ysumit99@gmail.com>
2019-08-12 00:59:11 +0530

    ZOMCAV aug long 4th question brute force  - WA

--- CodeChef/LongChallenge/AugLongChallenge2019B/ZOMCAV.cpp
+++ CodeChef/LongChallenge/AugLongChallenge2019B/ZOMCAV.cpp
@@ -25,115 +25,45 @@ int main(){
     while(t--)
     {
         cin >> n;
-        vector<ll> radiation(n+1), health(n+1);
 
-        set<ll> s;
-        set<ll>:: iterator itr;
+        //vector<int> radiation(n);
+        vector<int> health(n);
+        int radiation; 
+        vector<int> finalRadiation(n,0);
 
-        ll max_limit = 0;
-
-        for(int i = 1; i <= n; i++)
-            {
-                cin >> radiation[i];
-
-                 if(i - radiation[i] < 0)
-                {
-                max_limit = i + radiation[i] > n ? n : i + radiation[i];
-
-                    s.insert(max_limit);
-                    cout << "inserting " << max_limit << endl;
-                }
-                else
-                {
-                    max_limit = 2*radiation[i] > n ? n : 2*radiation[i];
-                    s.insert(max_limit);
-                    cout << "Inserting " << max_limit << endl;
-                }
-
-            }
-
-        for(int i = 0; i < n; i++)
-            cin >> health[i];
-
-        sort(health.rbegin(), health.rend());
-
-        // for(ll ele: health)
-        //     cout << "element = " << ele << "\n";
-
-        // for(int i = 1; i <= n; i++)
-        //     cout << radiation[i] << " ";
-
-        // cout << "\n";
-
-        // for(int i = 0; i < n; i++)
-        //     cout << health[i] << " ";
-
-        // cout << "\nend of test case\n";
-
-      
-
-       
-        
-        // cout << "set after sorting: \n";
-        // cout << "set size = " << s.size() << endl;
-
-        // set<ll>:: iterator itr;
-        cout << "start of set iteration\n";
-
-        for(itr = s.begin(); itr != s.end(); itr++)
-            cout << *itr << "\n";
-
-        cout << "end of set iteration\n";
-
-        itr = s.begin();
-        ll expected_val = n, prev;
-        int flag = 0, k = 0;
-
-        while(itr != s.end())
+        for (size_t i = 0; i < n; i++)
         {
-            //cout << *itr << endl;
+            cin >> radiation;
+            int minLimit = i + 1 - radiation <= 0 ? 1 : i + 1 - radiation;
+            int maxLimit = i + 1 + radiation > n ? n : i + 1 + radiation;
 
-            if(itr == s.begin())
-            {
-                for(int j = 0; j < *itr; j++)
-                {
-                    if(health[k] != expected_val)
-                        {
-                            flag = 1;
-                            break;
-                        }
-                    k++;
-                }
-                if(flag == 1)
-                    break;
-                
-                
-            }
-            else
-            {
-                for(int j = 0; j < *itr - prev; j++)
-                {
-                    if(health[k] != expected_val)
-                        {
-                            flag = 1;
-                            break;
-                        }
-                    k++;
-                }
-                if(flag == 1)
-                    break;
-                
-            }
-            prev = *itr;
-            expected_val--;
-            itr++;
+            for (size_t j = minLimit ; j <= maxLimit; j++)
+                finalRadiation[j-1]++;
             
         }
 
-        if(flag == 1)
-            cout << "NO\n";
-        else
+        for (size_t i = 0; i < n; i++)
+            cin >> health[i];
+        
+        sort(health.begin(), health.end());
+        sort(finalRadiation.begin(), finalRadiation.end());
+
+        bool allDead = true;
+
+        for(int i = 0; i < n; i++)
+            if(health[i] != finalRadiation[i])
+                allDead = false;
+        
+        if(allDead)
             cout << "YES\n";
+        else
+            cout << "NO\n";
+
+        
+        
+        
+
+      
     }
 
     return 0;
