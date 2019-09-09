43355fb0bd8b4432172230230bd0020fbd6e004b
ysumit99 <ysumit99@gmail.com>
2019-09-09 06:53:53 +0530

    Trying CHEFINSQ and created myTemplate

---
+++ myTemplate.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+/* factorial */
+ll factorial(ll n)
+{
+    ll result = 1;
+
+    while(n >= 1)
+        result *= n--;
+
+    return result;
+}
+
+/* Combination =>  nCr */
+ll combination(ll n, ll r)
+{
+    return factorial(n)/(factorial(n-r)*factorial(r));
+}
+
+int main()
+{
+
+    #ifdef DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+
+    cin >> t;
+
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
